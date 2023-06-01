8a2b6a7219f6f3a50c9b11a26cdab2c628dd7206
YebiDepriansyah <131685598+YebiDepriansyah@users.noreply.github.com>
2023-06-01 17:02:38 +0700

    Create Tugas 2.py

---
+++ Tugas 2.py
@@ -0,0 +1,70 @@
+# Definisi kelas Mahasiswa yang memiliki atribut nama, nim, dan jurusan
+class Mahasiswa:
+
+# Method konstruktor yang menginisialisasi atribut nama, nim, dan jurusan dari objek mahasiswa
+    def __init__(self, nama, nim, jurusan):
+        self.nama = nama
+        self.nim = nim
+        self.jurusan = jurusan
+
+# Method yang menampilkan informasi nama, nim, dan nama jurusan dari objek mahasiswa
+    def tampilkan_info(self):
+        print("Nama: ", self.nama)
+        print("NIM: ", self.nim)
+        print("Jurusan: ", self.jurusan.nama_jurusan)
+
+# Definisi kelas Jurusan yang memiliki atribut nama_jurusan dan daftar_mahasiswa
+class Jurusan:
+
+# Method konstruktor yang menginisialisasi atribut nama_jurusan dan daftar_mahasiswa dari objek jurusan
+    def __init__(self, nama_jurusan):
+        self.nama_jurusan = nama_jurusan
+        self.daftar_mahasiswa = []
+
+# Method yang menambahkan objek mahasiswa ke dalam atribut daftar_mahasiswa dari objek jurusan
+    def tambah_mahasiswa(self, mahasiswa):
+        self.daftar_mahasiswa.append(mahasiswa)
+
+# Method yang menampilkan daftar nama mahasiswa yang terdaftar di objek jurusan
+    def tampilkan_daftar_mahasiswa(self):
+        print("Daftar Mahasiswa", self.nama_jurusan)
+        for mahasiswa in self.daftar_mahasiswa:
+            print("- ", mahasiswa.nama)
+
+# Definisi kelas Universitas yang memiliki atribut nama_universitas dan daftar_jurusan
+class Universitas:
+
+# Method konstruktor yang menginisialisasi atribut nama_universitas dan daftar_jurusan dari objek universitas
+    def __init__(self, nama_universitas):
+        self.nama_universitas = nama_universitas
+        self.daftar_jurusan = []
+
+# Method yang menambahkan objek jurusan ke dalam atribut daftar_jurusan dari objek universitas
+    def tambah_jurusan(self, jurusan):
+        self.daftar_jurusan.append(jurusan)
+
+# Method yang menambahkan objek jurusan ke dalam atribut daftar_jurusan dari objek universitas
+    def tampilkan_daftar_jurusan(self):
+        print("Daftar Jurusan di", self.nama_universitas)
+        for jurusan in self.daftar_jurusan:
+            print("- ", jurusan.nama_jurusan)
+
+
+
+# 2. Membuat objek Universitas dengan nama "XYZ University"
+universitas_xyz = Universitas("XYZ University")
+
+# 3. Membuat objek Jurusan dengan nama "Teknik Informatika" dan menambahkannya ke dalam Universitas XYZ
+jurusan_ti = Jurusan("Teknik Informatika")
+universitas_xyz.tambah_jurusan(jurusan_ti)
+
+# 4. Membuat objek Mahasiswa dengan nama "Kalian masing" dan NIM "Kalian masing",
+#    lalu memasukkannya ke dalam Jurusan Teknik Informatika di Universitas XYZ
+mahasiswa_kalian = Mahasiswa("Yebi Depriansyah", "G1A022063", jurusan_ti)
+jurusan_ti.tambah_mahasiswa(mahasiswa_kalian)
+
+# 5. Menampilkan daftar jurusan yang ada di Universitas XYZ
+universitas_xyz.tampilkan_daftar_jurusan()
+
+# 6. Menampilkan daftar mahasiswa yang terdaftar dalam Jurusan Teknik Informatika di Universitas XYZ
+jurusan_ti.tampilkan_daftar_mahasiswa()
